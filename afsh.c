e6126162e9b2ea704a2b357b27130f49da739a35
Ian Goodbody <Ian Goodbody>
2016-02-02 20:19:19 -0800

    code should be complete

--- afsh.c
+++ afsh.c
@@ -7,104 +7,126 @@
 #include "list.h"
 #include "afsh.h"
 
+/*
+ * int main: Starts and runs the Air Force Shell.
+ * Author: C1C Ian Goodbody
+ * Date: 2 Feb 2015
+ */
 int main(void){
 
+    // Allocate memory for function buffers and objects
     char* inputStr      = (char*) malloc(STR_MAX);
     char* cleanInputStr = (char*) malloc(STR_MAX);
     char* cwdStr        = (char*) malloc(STR_MAX);
-    char* homeStr       = (char*) malloc(STR_MAX);
     char** argList      = (char**) malloc(ARG_ARRAY_MAX);
     node* historyList = NULL;
-
-    strcpy(homeStr,  "/home/");
-    if(getlogin_r(homeStr + 6, STR_MAX - 7));
-        printf("Could not retrieve user login.\n");
 
     printf("\nWelcome to the Air Force Shell (afsh)\n\n");
     while(1) {
 
+        // Get input from user
         printf("Prompt> ");
         fgets(inputStr, STR_MAX, stdin);
         parseInput(inputStr, argList, ARG_ARRAY_MAX);
 
+        // Check for no command entered, skip the rest of the loop and do not process anything.
         if(!*argList)
             continue;
 
-        if(strcmp(*argList, "history") && strcmp(*argList, "recall")) {
+        // If the commands are not "history", "exit", or "recall" add the command to the history list
+        if(strcmp(*argList, "history") && strcmp(*argList, "recall") && strcmp(*argList, "exit")) {
             cleanInput(cleanInputStr, argList);
             historyList = list_remove(historyList, cleanInputStr);
             historyList = list_insert_head(historyList, cleanInputStr);
         }
 
-        if(execArgs(argList, historyList, cwdStr, homeStr)) {
+        //  Execute the argument in the list, check for exit condition
+        if(execArgs(argList, historyList, cwdStr)) {
+            // if execArgs returns 1 for exit clear allocated memory and exit the program
             free(inputStr);
             free(cleanInputStr);
             free(argList);
             free(cwdStr);
-            free(homeStr);
             list_destroy(historyList);
             exit(0);
         }
     }
 }
 
-int execArgs(char** argList, node* historyList, char* cwdStr, char *homeStr){
-    if(!*argList)
-        return 0;
-    else if (!strcmp(*argList, "history")) {
-        printf("\nCommand History:\n");
-        // Set the iteration length
-        int i = list_length(historyList); // longest possible list length
+int execArgs(char** argList, node* historyList, char* cwdStr){
 
+    // Process "history" command
+    if (!strcmp(*argList, "history")) {
+        printf("\nCommand History:\n");
+        // Set the maximum iteration length
+        int i = list_length(historyList);
+        // User entered no lengh command, set default to 10 or longest possible
         if(!*(argList+1)){
             if(i > 10)
                 i = 10;
         }
-        else if( i > atoi(*(argList+1))) // Use user specified if there more than that number to show
+        else if( i > atoi(*(argList+1))) // set to user specified length or the logest possible
             i = atoi(*(argList+1));
 
+        // Iteratively print the list tail to head and add numbers
         for( ; i > 0; i--){
             printf("%i.\t", i);
             list_printn(historyList, i);
         }
         return 0;
     }
+
+    // process the "recall" command
     else if (!strcmp(*argList, "recall")) {
-        int index;
-        char* recallStr;
+        int index; // The index of the command to be recalled
+        char* recallStr; // Command recalled from the list
 
         if(*(argList+1))
-            index = atoi(*(argList+1));
+            index = atoi(*(argList+1)); // Set index to the user input
         else
-            index = 1;
+            index = 1; // Default is index 1
+
         // Check that the list returned a value
         if((recallStr = list_get(historyList, index))) {
             printf("Executing: %s:\n", recallStr);
+            // Execute the new command
             parseInput(recallStr, argList, ARG_ARRAY_MAX);
-            execArgs(argList, historyList, cwdStr, homeStr);
+            execArgs(argList, historyList, cwdStr);
         }
         return 0;
     }
+    // process the "cd"
     else if (!strcmp(*argList, "cd")) {
-        if(!strcmp(*(argList+1), "~"))
-            chdir(homeStr);
-        else
-            chdir(*(argList+1));
-        printf("Directory changed to: %s\n", getcwd(cwdStr, STR_MAX));
+        // User no directory, just move on
+        if(!*(argList+1));
+        // process the "home" call
+        else if(!strcmp(*(argList+1), "~"))
+            chdir(getenv("HOME"));
+        // Process user defined call
+        else {
+            // check if chdir returns an error
+            if(chdir(*(argList + 1)))
+                printf("Directory does not exist.\n");
+        }
+        printf("cwd=> %s\n", getcwd(cwdStr, STR_MAX));
         return 0;
     }
+    // Process the "exit" command
     else if (!strcmp(*argList, "exit")) {
         return 1;
     }
+    // process not recognized, pass the command on to the system.
     else {
         pid_t forkedID = fork();
         if (forkedID < 0)
-            printf("Fork failed.\n");
+            printf("Fork failed\n");
         else if (forkedID > 0) {
             wait(NULL);
         }
         else {
-            execvp(*argList, argList);
+            // Check if the system call returned an error
+            if(execvp(*argList, argList))
+                printf("Command not found\n");
             exit(0);
         }
         return 0;
@@ -112,17 +134,15 @@ int execArgs(char** argList, node* historyList, char* cwdStr, char *homeStr){
 }
 
 char* cleanInput(char* nString, char** argArray){
-    char* sString = nString;
+    char* sString = nString; // Save the pointer to the head of the string to be returned
     while(*argArray){
         int i = 0;
-        while(*(*argArray + i)){
-            *nString++ = *(*argArray+i);
-            i++;
-        }
-        *nString++ = ' ';
+        while(*(*argArray + i))
+            *nString++ = *(*argArray+i++); // copy the data from argArray to nString with the power of C
+        *nString++ = ' '; // Add only one space between each argument
         argArray++;
     }
-    *nString = '\0';
+    *nString = '\0'; // Terminate teh string
     return sString;
 }
 
--- afsh.h
+++ afsh.h
@@ -3,11 +3,40 @@
 #ifndef PEX1_AFSH_H
 #define PEX1_AFSH_H
 
-#define BUFF_MAX 4096
-#define ARG_MAX 64
+#define STR_MAX sizeof(char) * 1024
+#define ARG_ARRAY_MAX sizeof(char*) * 64
 
-void history(node* histHead);
-char* recall(node* histHead, char* item);
-char** parseInput(char*, char**, size_t argSize);
+/*
+ * execArgs: Executes the arguments in argList. Seperate function allows for recursive use of recall.
+ * Parameters:
+ *  - argList: char** a NULL terminated array of string pointers
+ *  - historyList: node* pointer to the head of the linked list storing the history of all commands
+ *      Note that because the pointers is pass-by-value the list cannot be changed in this function
+ *  - cwdStr: char* The buffer where the program stores the current directory
+ *  Returns: 1 as the exit condition, 0 for the continue condition
+ */
+int execArgs(char** argList, node* historyList, char* cwdStr);
+
+/*
+ * cleanInput: constructs a cleanly formatted string from an arguments array
+ * Parameters:
+ *  - nString: char* a pointer to the buffer where the cleaned up string is stored
+ *  - argArray: char** pointer to the arguments list
+ * Returns: char* pointer to the formatted string
+ */
+char* cleanInput(char* nString, char** argArray);
+
+/*
+ * parseInput: converts the raw console stirng into an array of arguments that can be read by the interpreter
+ *  The args list created by this function is an array containing pointers to the data in the input buffer. The
+ *  data in the buffer is injected with null values in order to seperate the string. Therefor argArray will be invalid
+ *  after the inBuff is reset.
+ * Parameters:
+ *  - inBuff: char* pointer to the raw string
+ *  - argArray: char** pointer to the arguments list where the pointers will be stored
+ *  - argSize: size_t the size of the array in bytes
+ *  Returns: char** pointer to the list of arguments.
+ */
+char** parseInput(char* inBuff, char** argArray, size_t argSize);
 
 #endif //PEX1_AFSH_H
